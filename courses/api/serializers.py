225bffc023571650729e74a1991e07e7316d86e2
nariman <nariman.h98@gmail.com>
2025-02-15 20:23:34 +0330

    initail commit

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,10 +1,39 @@
+from django.db.models import Count
 from rest_framework import serializers
-from courses.models import Subject
+from courses.models import Subject, Course
 
 class SubjectSerializer(serializers.ModelSerializer):
     total_courses = serializers.IntegerField()
+    popular_courses = serializers.SerializerMethodField()
+
+    def get_popular_courses(self, obj):
+        courses = obj.courses.annotate(total_students=Count('students')).order_by('total_students')[:3]
+        return [
+            f'{c.title} {c.total_students} ' for c in courses
+        ]
     class Meta:
         model = Subject
-        fields = ['id', 'title', 'slug', 'total_courses']
+        fields = ['id',
+                  'title',
+                  'slug',
+                  'total_courses',
+                  'popular_courses'
+                  ]
+
+
+class CourseSerializer(serializers.ModelSerializer):
+        modules = serializers.StringRelatedField(many=True, read_only=True)
+        class Meta:
+            model = Course
+            fields = [
+                'id',
+                'subject',
+                'title',
+                'slug',
+                'overview',
+                'created',
+                'owner',
+                'modules',
+            ]
 
 
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,14 +1,32 @@
+from http.client import responses
+
 from rest_framework import generics
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from courses.api.serializers import SubjectSerializer, CourseSerializer
+from courses.models import Subject, Course
+from courses.api.pagination import StandardPagination
 from django.db.models import Count
 
 
-class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.annotate(total_courses=Count('courses'))
-    serializer_class = SubjectSerializer
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
 
 
-class SubjectDetailView(generics.RetrieveAPIView):
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
+
+
+class CourseEnrollView(APIView):
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
